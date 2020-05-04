4061bf882917eb046756a6c64f32b08f6d490356
star-001 <fanzhenxin@outlook.com>
2020-05-04 21:24:54 +0800

    init .dotfiles

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,8 @@
+alias vim='nvim'
+alias vi='nvim'
+alias Syu='sudo pacman -Syu'
+alias svim='sudo nvim'
+alias less='less -R'
+
+set -U fish_user_paths /home/fan/.gem/ruby/2.7.0/bin $fish_user_paths
+set fish_greeting
