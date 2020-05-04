e45005126b310e00ebd123a2be900ea27a4f7f4b
star-001 <fanzhenxin@outlook.com>
2020-05-04 21:36:31 +0800

    add comment in config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,5 +4,7 @@ alias Syu='sudo pacman -Syu'
 alias svim='sudo nvim'
 alias less='less -R'
 
+# 设置path
 set -U fish_user_paths /home/fan/.gem/ruby/2.7.0/bin $fish_user_paths
+set -x EDITOR /usr/bin/nvim
 set fish_greeting
