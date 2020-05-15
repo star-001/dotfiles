c6bde6c9f1ef749dbc85da496940419960157c60
star-001 <fanzhenxin@outlook.com>
2020-05-15 22:48:25 +0800

    update i3config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,8 +4,10 @@ alias Syu='sudo pacman -Syu'
 alias Sua='yay -Sua'
 alias svim='sudo nvim'
 alias less='less -R'
+alias psg='ps aux|grep'
+alias disownj="disown (jobs|awk -F ' ' '{print$2}')"
 
-# 设置path
+# 设置path test
 set -U fish_user_paths /home/fan/.gem/ruby/2.7.0/bin $fish_user_paths
 set -x EDITOR /usr/bin/nvim
 set fish_greeting
